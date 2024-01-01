fcc1168862e6d149b088d87511e4bb5331f5554d
prajeshElEvEn <prajesh.eleven118@gmail.com>
2024-01-02 00:42:40 +0530

    added shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="">
       <h1>hi</h1>
+      <Button variant={"link"}>Click</Button>
     </main>
   );
 }
